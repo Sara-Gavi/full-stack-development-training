f0df1ad1ee11519eceb562e9162b1373bb29aae4
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-11-26 20:13:02 +0100

    Created and configured App component with routes and default navigation

--- ludoteca-react/src/App.tsx
+++ ludoteca-react/src/App.tsx
@@ -1,19 +1,21 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Game } from "./pages/Game/Game";
+import { Author } from "./pages/Author/Author";
+import { Category } from "./pages/Category/Category";
+import { Layout } from "./components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const probando = "probando 123";
-
   return (
-    <>
-      <p>{probando}</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="games" element={<Game />} />
+          <Route path="categories" element={<Category />} />
+          <Route path="authors" element={<Author />} />
+          <Route path="*" element={<Navigate to="/games" />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
