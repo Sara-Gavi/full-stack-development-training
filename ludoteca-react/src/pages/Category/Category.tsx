a30b4e480c9976d8e9f6a06a1f0fed5348a352b7
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-11-27 19:42:01 +0100

    Add mock Category component with table and basic actions

--- ludoteca-react/src/pages/Category/Category.tsx
+++ ludoteca-react/src/pages/Category/Category.tsx
@@ -1,3 +1,78 @@
+import { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import ClearIcon from "@mui/icons-material/Clear";
+import IconButton from "@mui/material/IconButton";
+import styles from "./Category.module.css";
+import { Category as CategoryModel } from "../../types/Category";
+
 export const Category = () => {
-  return <div>Category</div>;
+  const data = [
+    {
+      id: "1",
+      name: "Test 1",
+    },
+    {
+      id: "2",
+      name: "Test 2",
+    },
+  ];
+
+  return (
+    <div className="container">
+      <h1>Listado de Categorías</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead
+            sx={{
+              "& th": {
+                backgroundColor: "lightgrey",
+              },
+            }}
+          >
+            <TableRow>
+              <TableCell>Identificador</TableCell>
+              <TableCell>Nombre categoría</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((category: CategoryModel) => (
+              <TableRow
+                key={category.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {category.id}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                  {category.name}
+                </TableCell>
+                <TableCell>
+                  <div className={styles.tableActions}>
+                    <IconButton aria-label="update" color="primary">
+                      <EditIcon />
+                    </IconButton>
+                    <IconButton aria-label="delete" color="error">
+                      <ClearIcon />
+                    </IconButton>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className="newButton">
+        <Button variant="contained">Nueva categoría</Button>
+      </div>
+    </div>
+  );
 };
