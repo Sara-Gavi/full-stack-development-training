093d16616e0554dd3395c8f892beb5d67732af4d
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-12-15 19:32:59 +0100

    Fix: Updated import path for setupListeners to use '@reduxjs/toolkit/query' for better compatibility

--- ludoteca-react/src/redux/store.ts
+++ ludoteca-react/src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { setupListeners } from "@reduxjs/toolkit/query"; //import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { ludotecaAPI } from "./services/ludotecaApi";
 
 export const store = configureStore({
