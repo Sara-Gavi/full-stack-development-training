c48e5d5269a215cdfd3029475074757c5b87dc86
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-12-15 19:23:05 +0100

    Setup listeners and define RootState and AppDispatch types

--- ludoteca-react/src/redux/store.ts
+++ ludoteca-react/src/redux/store.ts
@@ -9,3 +9,10 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([ludotecaAPI.middleware]),
 });
+
+setupListeners(store.dispatch);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
