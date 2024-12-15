0084642419eedad23cab81169d75a6bb0ac7cb91
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-12-15 19:21:23 +0100

    Configure Redux store with ludotecaAPI reducer and middleware

--- ludoteca-react/src/redux/store.ts
+++ ludoteca-react/src/redux/store.ts
@@ -1,3 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { ludotecaAPI } from "./services/ludotecaApi";
+
+export const store = configureStore({
+  reducer: {
+    [ludotecaAPI.reducerPath]: ludotecaAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([ludotecaAPI.middleware]),
+});
