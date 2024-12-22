a26f1bf4f3032debaca18e45c4e5cc8568af0a2c
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-12-22 18:49:30 +0100

    feat: Integrated Redux Provider in App.tsx to enable global state access

--- ludoteca-react/src/App.tsx
+++ ludoteca-react/src/App.tsx
@@ -3,19 +3,23 @@ import { Game } from "./pages/Game/Game";
 import { Author } from "./pages/Author/Author";
 import { Category } from "./pages/Category/Category";
 import { Layout } from "./components/Layout";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route index path="games" element={<Game />} />
-          <Route path="categories" element={<Category />} />
-          <Route path="authors" element={<Author />} />
-          <Route path="*" element={<Navigate to="/games" />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route index path="games" element={<Game />} />
+            <Route path="categories" element={<Category />} />
+            <Route path="authors" element={<Author />} />
+            <Route path="*" element={<Navigate to="/games" />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
